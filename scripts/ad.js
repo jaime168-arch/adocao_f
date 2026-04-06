9724e8e6ffe33ba36ab8e2c0e54c7f243faa362e
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-04-06 12:44:10 -0300

    add- js

--- scripts/ad.js
+++ scripts/ad.js
@@ -3,6 +3,8 @@ document.getElementById("formAdocao").addEventListener("submit", function (e){
 
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let CPF = document.getElementById("cpf").value;
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
 
