23f38f0e2199cd4aed8da93d91021535cfaafbb3
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-04-08 07:35:44 -0300

    add- alert idade

--- scripts/ad.js
+++ scripts/ad.js
@@ -7,8 +7,15 @@ document.getElementById("formAdocao").addEventListener("submit", function (e){
     let CPF = document.getElementById("cpf").value;
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
+    
 
     if(nome.length < 3) return alert("Nome inválido");
+    if (idade < 18){
+        alert("Você tem que ter 18 para adotar ou até mais");
+        return 0;
+    }
+
+    
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: "
     + nome;
