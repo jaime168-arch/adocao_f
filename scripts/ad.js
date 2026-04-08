d97a05a8e96bf95680fb59d49dca42aaa10c470c
Jaime Rodrigues <jaime_r_pereira@estudante.sesisenai.org.br>
2026-04-08 11:42:53 -0300

    add- site obviamente concluido

--- scripts/ad.js
+++ scripts/ad.js
@@ -1,8 +1,13 @@
 document.getElementById("formAdocao").addEventListener("submit", function (e) {
     e.preventDefault();
     
+   
+    const imagemPet = document.getElementById("golden.png.png");
+    
     
     const cpfCadastrado = ["123.456.789-00", "000.111.222-33"];
+    
+    
     const nome = document.getElementById("nome").value;
     const email = document.getElementById("email").value;
     const telefone = document.getElementById("tel").value;
@@ -13,30 +18,28 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     const quintalElemento = document.querySelector('input[name="quintal"]:checked');
     const quintal = quintalElemento ? quintalElemento.value : "nao";
 
+   
+
     if (nome.length < 3) {
         alert("Nome inválido. Digite seu nome completo.");
         return;
     }
-
 
     if (isNaN(idade) || idade < 18) {
         alert("Você precisa ter 18 anos ou mais para adotar.");
         return;
     }
 
-  
     if (cpfCadastrado.includes(cpf)) {
         alert("Este CPF já possui uma solicitação no nosso sistema.");
         return;
     }
 
-    
     if (moradia === "apartamento" && quintal === "sim") {
         alert("Erro: Apartamentos padrão não possuem quintal externo.");
         return;
     }
 
-    
     if (horas > 8) {
         const justifica = prompt("O animal ficaria mais de 8h sozinho. Como você pretende lidar com isso? (Justificativa obrigatória)");
         
@@ -46,11 +49,20 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
         }
     }
 
+    if (imagemPet) {
+        imagemPet.style.border = "5px solid #fff"; 
+        imagemPet.style.transform = "scale(1.1)"; 
+    }
 
     const resultadoDiv = document.getElementById("resultado");
     resultadoDiv.style.color = "#adff2f"; 
-    resultadoDiv.innerHTML = `<h3>✅ Cadastro realizado com sucesso!</h3>
-                              <p><strong>Nome:</strong> ${nome}</p>
-                              <p>Nossa equipe entrará em contato em breve.</p>`;
-
+    resultadoDiv.style.marginTop = "20px";
+    resultadoDiv.style.textAlign = "center";
+    
+    resultadoDiv.innerHTML = `
+        <h3>✅ Cadastro realizado com sucesso!</h3>
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p>Sua solicitação para adotar o Golden foi enviada!</p>
+        <p>Nossa equipe entrará em contato em breve.</p>
+    `;
 });
